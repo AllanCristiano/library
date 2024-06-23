f97f550b5d70700991306cfb11057acddbeb7abc
Allan Cristiano da Silva Santos <drmundo920@gmail.com>
2024-06-23 10:21:14 -0300

    feat: delete router author

--- src/main/java/allancristiano/library/controller/AuthorController.java
+++ src/main/java/allancristiano/library/controller/AuthorController.java
@@ -14,6 +14,7 @@ import allancristiano.library.services.AuthorService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -60,6 +61,7 @@ public class AuthorController {
         }
     }
 
+    @DeleteMapping({"id"})
     public ResponseEntity<Void> deleteAuthor(@PathVariable UUID id) {
         try {
             authorService.deleteAuthor(id);
